5ee88753b6de0ae27b3f127560d09673234cdd18
firepunch0600 <44721517+firepunch0600@users.noreply.github.com>
2018-11-03 17:00:03 +0900

    Add files via upload
    Day1

---
+++ Assets/Script/PLAYER/FSMManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState
+{
+    IDLE = 0,
+    RUN,
+    CHASE,
+    ATTACK
+}
+
+public class FSMManager : MonoBehaviour
+{
+    public PlayerState currentState;
+    public PlayerState startState;
+    public Transform marker;
+
+    Dictionary<PlayerState, PlayerFSMState> states = new Dictionary<PlayerState, PlayerFSMState>() ;
+
+    private void Awake()
+    {
+        marker = GameObject.FindGameObjectWithTag("Marker").transform;
+
+        states.Add(PlayerState.IDLE, GetComponent<playerIDLE>());
+        states.Add(PlayerState.RUN, GetComponent<playerRUN>());
+        states.Add(PlayerState.CHASE, GetComponent<playerCHACE>());
+        states.Add(PlayerState.ATTACK, GetComponent<playerATTACK>());
+
+       // states[startState].enabled = true;
+    }
+
+    public void SetState(PlayerState newState) {
+        foreach (PlayerFSMState fsm in states.Values)
+        {
+            fsm.enabled = false;
+        }
+
+        states[newState].enabled = true;
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+        SetState(startState);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(r, out hit, 1000))
+            {
+                marker.position = hit.point;
+                Debug.Log(hit.point);
+               SetState(PlayerState.RUN);
+
+
+            }
+
+
+            //Debug.Log("click" + Input.mousePosition);
+        }
+    }
+}
